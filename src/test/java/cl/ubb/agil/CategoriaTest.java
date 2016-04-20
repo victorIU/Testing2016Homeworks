1c3fd6e3f4965883a73a6b0d5fda7fe62b125b8f
Victor Inostroza Urrutia <Victor Inostroza Urrutia>
2016-04-20 16:17:09 -0300

    primer commit

---
+++ src/test/java/cl/ubb/agil/CategoriaTest.java
@@ -0,0 +1,13 @@
+package cl.ubb.agil;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+public class CategoriaTest {
+	@Test
+	
+	
+	
+	
+}
